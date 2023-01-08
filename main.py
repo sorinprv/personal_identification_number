447fc43af2944ecadf50a5ae875e165d693576cc
sorin pirvu <sorin_pirvu@yahoo.com>
2023-01-08 14:12:57 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	ciorna.py
    #	ciorna2.py
    #	main.py
    #	work.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from cnp import cnp
 from work import total
 
+"""Program made for the verification and decoding of the personal numerical code (cnp),
+        in order to verify the data on the IC with those from the personal numerical code (cnp)."""
+
 print(total(cnp))
